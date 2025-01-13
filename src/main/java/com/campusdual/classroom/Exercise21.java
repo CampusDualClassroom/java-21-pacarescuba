f48974c12da2620b2fe2323155a70aee4e425a8f
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2025-01-13 11:52:45 +0100

    Added solution to Exercise21

--- src/main/java/com/campusdual/classroom/Exercise21.java
+++ src/main/java/com/campusdual/classroom/Exercise21.java
@@ -6,25 +6,51 @@ import java.util.TreeSet;
 
 public class Exercise21 {
 
-    public static Set<String> createHashSet() {
+  public static Set<String> createHashSet() {
+    Set<String> set = new HashSet<>();
+    set.add("ELEMENT KPRBC");
+    set.add("ELEMENT YPBTM");
+    set.add("ELEMENT AADXU");
+    set.add("ELEMENT RXCGJ");
+    set.add("ELEMENT WYMVD");
+    set.add("ELEMENT WFGEJ");
+    set.add("ELEMENT TYGBS");
+    set.add("ELEMENT MAPTK");
+    set.add("ELEMENT GJXVE");
+    set.add("ELEMENT BAFGL");
+    return set;
+  }
 
+  public static Set<String> createTreeSet() {
+    Set<String> set = new TreeSet<>();
+    set.add("ELEMENT KPRBC");
+    set.add("ELEMENT YPBTM");
+    set.add("ELEMENT AADXU");
+    set.add("ELEMENT RXCGJ");
+    set.add("ELEMENT WYMVD");
+    set.add("ELEMENT WFGEJ");
+    set.add("ELEMENT TYGBS");
+    set.add("ELEMENT MAPTK");
+    set.add("ELEMENT GJXVE");
+    set.add("ELEMENT BAFGL");
+    return set;
+  }
+
+  private static void printSet(Set<String> customSet) {
+    for (String s : customSet) {
+      System.out.println(s);
     }
-
-    public static Set<String> createTreeSet() {
-
-    }
-
-    private static void printSet(Set<String> customSet) {
-
-    }
+  }
 
 
-    public static boolean addElementToSet(Set<String> set, String elementToAdd) {
+  public static boolean addElementToSet(Set<String> set, String elementToAdd) {
+    return set.add(elementToAdd);
+  }
 
-    }
-
-    public static void main(String[] args) {
-
-    }
+  public static void main(String[] args) {
+    printSet(createHashSet());
+    System.out.println();
+    printSet(createTreeSet());
+  }
 
 }
